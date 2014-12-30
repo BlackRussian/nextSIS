74c1b0dbdbdbc06a37780262892a01ff9157c847
BlackRussian <omariecase@hotmail.com>
2014-12-30 11:37:50 -0500

    Template Header and Footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,14 +1,14 @@
   			</div>
   		</div>
   		
-  		<!--link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-    	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script-->
+  		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+    	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 
-		    <link href="/assets/vendors/datepicker.css" rel="stylesheet" media="screen">
+		<link href="/assets/vendors/datepicker.css" rel="stylesheet" media="screen">
         <link href="/assets/vendors/uniform.default.css" rel="stylesheet" media="screen">
         <link href="/assets/vendors/chosen.min.css" rel="stylesheet" media="screen">
 
-        <!--link href="/assets/vendors/wysiwyg/bootstrap-wysihtml5.css" rel="stylesheet" media="screen"-->
+        <link href="/assets/vendors/wysiwyg/bootstrap-wysihtml5.css" rel="stylesheet" media="screen">
 
         <script src="/assets/vendors/jquery-1.9.1.min.js"></script>
         <script src="/assets/js/bootstrap.min.js"></script>
@@ -17,13 +17,13 @@
         <script src="/assets/vendors/bootstrap-datepicker.js"></script>
         <script src="/assets/vendors/datatables/js/jquery.dataTables.min.js"></script>
         
-        <!--script src="/assets/vendors/wysiwyg/wysihtml5-0.3.0.js"></script-->
-        <!--script src="/assets/vendors/wysiwyg/bootstrap-wysihtml5.js"></script-->
-        <!--script src="/assets/vendors/wizard/jquery.bootstrap.wizard.min.js"></script-->
-    		<!--script src="/assets/vendors/jquery-validation/dist/jquery.validate.min.js"></script>
+        <script src="/assets/vendors/wysiwyg/wysihtml5-0.3.0.js"></script>
+        <script src="/assets/vendors/wysiwyg/bootstrap-wysihtml5.js"></script>
+        <script src="/assets/vendors/wizard/jquery.bootstrap.wizard.min.js"></script>
+    	<!--script src="/assets/vendors/jquery-validation/dist/jquery.validate.min.js"></script>
     		
-    		<script src="/assets/js/form-validation.js"></script-->
-    		<script src="/assets/js/scripts.js"></script>
+    	<script src="/assets/js/form-validation.js"></script-->
+    	<script src="/assets/js/scripts.js"></script>
         <script src="/assets/js/DT_bootstrap.js"></script>
         <!--script>
     			jQuery(document).ready(function() {   
