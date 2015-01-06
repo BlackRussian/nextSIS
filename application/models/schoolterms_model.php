8c9780cc3bbb2add1fc14f926489aa4379ca5dcd
nitro2001 <chinds@tumpit.com>
2015-01-06 11:39:34 -0500

    School Terms
    Add and listing for school terms

--- application/models/schoolterms_model.php
+++ application/models/schoolterms_model.php
@@ -26,7 +26,32 @@ class Schoolterms_model extends CI_Model
 	public function listing($schoolid, $syear)
  	{
 		// select all the information from the table we want to use with a 10 row limit (for display)
-		$this->db->select('id,school_id,title,syear,startdate,enddate')->from('term')->where('school_id',$schoolid)->where('syear',$syear)->limit(10);
+		$sql = "Select a.marking_period_id,a.school_id,a.title,a.syear,a.start_date,a.end_date,
+				GROUP_CONCAT(b.title SEPARATOR ', ') as Terms from school_year a
+				left outer join school_semester b on a.marking_period_id = b.year_id
+				group by a.marking_period_id";
+		//$this->db->select('marking_period_id,school_id,title,syear,start_date,end_date')->from('school_semester')->where('school_id',$schoolid)->where('syear',$syear)->limit(10);
+
+   		// run the query and return the result
+   		//$query = $this->db->get();
+		$query = $this->db->query($sql);
+		// proceed if records are found
+   		if($query->num_rows()>0)
+   		{
+			// return the data (to the calling controller)
+			return $query->result();
+   		}
+		else
+		{
+			// there are no records
+			return FALSE;
+		}
+ 	}
+	
+	public function GetSchoolTermsBySchoolYearId($syearId)
+ 	{
+		// select all the information from the table we want to use with a 10 row limit (for display)
+		$this->db->select('marking_period_id,year_id,school_id,title,syear,start_date,end_date')->from('school_semester')->where('year_id',$syearId)->limit(10);
 
    		// run the query and return the result
    		$query = $this->db->get();
@@ -43,19 +68,112 @@ class Schoolterms_model extends CI_Model
 			return FALSE;
 		}
  	}
+
+	public function GetSchoolQuatersBySchoolYearId($syearId)
+	 	{
+			$sql = "Select a.marking_period_id,c.title as SchoolYearTitle,b.title as TermTitle,a.school_id,a.title,a.syear,
+					a.start_date,a.end_date from school_quarter a
+				inner join school_semester b on a.semester_id = b.marking_period_id
+				inner join school_year c on a.year_id = c.marking_period_id
+				where a.year_id = $syearId";
+	   		// run the query and return the result
+	   		//$query = $this->db->get();
+			$query = $this->db->query($sql);
+			// proceed if records are found
+	   		if($query->num_rows()>0)
+	   		{
+				// return the data (to the calling controller)
+				return $query->result();
+	   		}
+			else
+			{
+				// there are no records
+				return FALSE;
+			}
+	 	}
+	
+	//Manage marking period Id
+	public function getMarkingPeriodId()
+	{
+		//$this->db->select('fn_marking_period_seq()');
+		//$query = $this->db->get();
+		//$this->db->call_function('fn_marking_period_seq');
+		//$this->db->query("call fn_marking_period_seq()");
+		//$query = $this->db->query("select fn_marking_period_seq()");
+		//echo $query->result_array();
+		
+		//$vars = get_object_vars ( $query );
+		//print_r ($vars);
+		
+		
+		$data = array(
+		   'id' => NULL 
+		);
+		
+		$this->db->insert('marking_period_id_generator', $data); 
+		$id = $this->db->insert_id();
+		$this->db->flush_cache();
+		
+		return $id;
+	}
+	
+	public function PopulateSchoolTermsDDLByyearId($syearId)
+ 	{
+		$sql = "Select a.marking_period_id,CONCAT(a.Title,' - ', a.start_date) as Title
+				from school_semester a
+				where a.year_id =$syearId";
+   		// run the query and return the result
+   		//$query = $this->db->get();
+		$query = $this->db->query($sql);
+		// proceed if records are found
+   		if($query->num_rows()>0)
+   		{
+			// return the data (to the calling controller)
+			return $query->result();
+   		}
+		else
+		{
+			// there are no records
+			return FALSE;
+		}
+ 	}
+	
+	
+	//Add new school year
+	public function addschoolyear($data)
+ 	{
+ 		//This section will be used to add the person data
+ 		
+		$this->db->insert('school_year', $data);
+		$id = $this->db->insert_id();
+		$this->db->flush_cache();
+		//echo "marking period Id: " . $id;
+		return $id;
+ 	}
+	
+	
+	
 	
 	//Add person model
  	public function addschoolterms($data)
  	{
  		//This section will be used to add the person data
  		
-		$this->db->insert('term', $data);
+		$this->db->insert('school_semester', $data);
 		
 		$this->db->flush_cache();
-		
-				
-		
  	}
+	
+	//Add person model
+ 	public function addtermquarter($data)
+ 	{
+ 		//This section will be used to add the person data
+ 		
+		$this->db->insert('school_quarter', $data);
+		
+		$this->db->flush_cache();
+ 	}
+	
 	
 	
 	
@@ -66,10 +184,16 @@ class Schoolterms_model extends CI_Model
  		$this->db->where('id', $id);
 		$this->db->update('term', $data);
 		$this->db->flush_cache();
-		
-		
-		
-		
+	
+ 	}
+	
+	public function updateschoolyear($id,$data)
+ 	{
+ 		//This section will be used to update the person data
+ 		$this->db->where('marking_period_id', $id);
+		$this->db->update('school_year', $data);
+		$this->db->flush_cache();
+	
  	}
  	//Get Grade Levels
 	public function GetGradeLevels($schoolid)
@@ -100,6 +224,28 @@ class Schoolterms_model extends CI_Model
  		
 		// select all the information from the table we want to use with a 10 row limit (for display)
 		$this->db->select('id,school_id,title,syear,startdate,enddate')->from('term')->where('id',$id);
+
+   		// run the query and return the result
+   		$query = $this->db->get();
+		
+		// proceed if records are found
+   		if($query->num_rows()>0)
+   		{
+			// return the data (to the calling controller)
+			return $query->result();
+   		}
+		else
+		{
+			// there are no records
+			return FALSE;
+		}
+ 	}
+	
+	public function GetSchoolYearById($id)
+ 	{
+ 		
+		// select all the information from the table we want to use with a 10 row limit (for display)
+		$this->db->select('marking_period_id,school_id,title,syear,short_name,start_date,end_date')->from('school_year')->where('marking_period_id',$id);
 
    		// run the query and return the result
    		$query = $this->db->get();
--- application/views/schoolterms/schoolterms_view.php
+++ application/views/schoolterms/schoolterms_view.php
@@ -1,94 +1,43 @@
-<!DOCTYPE html>
-<!--
- nextSIS person view
- 
- PURPOSE 
- This displays a list of people in the database.
- 
- LICENCE 
- This file is part of nextSIS.
- 
- nextSIS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
- published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-  
- nextSIS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-  
- You should have received a copy of the GNU General Public License along with nextSIS. If not, see
- <http://www.gnu.org/licenses/>.
-  
- Copyright 2012 http://nextsis.org
-
-
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">		
-		<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css');?>" type="text/css">
-		<link rel="stylesheet" href="<?php echo base_url('assets/css/nextsis.css');?>" type="text/css">
-		<link rel="shortcut icon" href="<?php echo base_url('assets/img/nextsis.favicon.png');?>">
-		<title><?php echo $this->lang->line('student_information_system');?></title>				
-	</head>
-	
-	<body>
-		<div class="navbar">
-  			<div class="navbar-inner">
-  				<div class="container">
-  				   	<a class="brand" href="home">nextSIS&gt;</a>
-  				   	<div class="navbar-content">
-    					<ul class="nav">
-    						<li><a href="home"><i class="icon-home"></i>&nbsp;<?php echo $this->lang->line("home");?></a></li>
-      						<li class="active"><a href=""><i class="icon-user"></i>&nbsp;<?php echo $this->lang->line("people");?></a></li>
-      						<li><a href=""><i class="icon-list-alt"></i>&nbsp;<?php echo $this->lang->line("courses");?></a></li>
-      						<li><a href=""><i class="icon-wrench"></i>&nbsp;<?php echo $this->lang->line("setup");?></a></li>
-      						<li><a href=""><i class="icon-question-sign"></i>&nbsp;<?php echo $this->lang->line("help");?></a></li>
-      						<li><a href="person/logout"><i class="icon-off"></i>&nbsp;<?php echo $this->lang->line("logout");?></a></li>
-  				   		</ul>
-    					<ul class="nav pull-right">
-      						<li><a href="person/me"><i class="icon-user"></i>&nbsp;<?php echo $username;?></a></li>
-    					</ul>  				   		
-  				   	</div>
-  				</div>
-  			</div>
-  		</div>-->
-  		
-  		<div class="container-fluid">
-  			<div class="row-fluid">
-  				<div class="span4 navleft">
-  					<ul class="nav nav-pills nav-stacked">
-  						<li ><a href=""><?php echo $this->lang->line("search");?>&nbsp;<i class="icon-search icon-white"></i></a></li>
-  						<li class="active"><a href="../schoolterms/listing"><?php echo $this->lang->line("school_terms");?></a></li> 	
-  						<li><a href="../gradelevels/listing"><?php echo $this->lang->line("grade_levels");?></a></li>  
-  						<li><a href="../schoolclasses/listing"><?php echo $this->lang->line("schoolclasses");?></a></li>
-  						<li><a href="../schoolperiods/listing"><?php echo $this->lang->line("school_periods");?></a></li> 	
-  						<li><a href="../schoolsubjects/listing"><?php echo $this->lang->line("school_subjects");?></a></li> 			  						
-  					</ul>
-  					<div class="well">
-  						<p><b><?php echo $this->lang->line("help");?></b>&nbsp;<?php echo $this->lang->line("sample_help_message");?></p>
-  					</div>
-  				</div>
-  				
-        		<div class="span8">
-					<h1><?php echo $this->lang->line("school_terms");?></h1>
-					<table>
+<div class="span9" id="content">
+	<div class="row-fluid">
+		<?php echo $this->load->view('templates/breadcrumb.php');?>
+     	<div class="block">
+       		<div class="block-content collapse in">
+          		<div class="span12">
+          			<?php $this->load->view('shared/display_notification.php');?>
+					<h3>People</h3>
+					<div class="table-toolbar">
+                    	<div class="btn-group">
+                    		<a href="add"><button class="btn btn-success"><?php echo $this->lang->line("add_new_schoolterm");?><i class="icon-plus icon-white"></i></button></a>
+                    	</div>
+					</div>	
+					<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered">
+								<thead>
 					<tr>
-						<td>
+						<th>
 							Year
-						</td>
-						<td>
+						</th>
+						<th>
 							Title
-						</td>
-						<td>
+						</th>
+						<th>
 							Start Date
-						</td>
-						<td>
+						</th>
+						<th>
 							End Date
-						</td>
+						</th>
+						<th>
+							Terms
+						</th>
 						
-						<td>
+						
+						<th>
 							&nbsp;
-						</td>
+						</th>
 					</tr>
+					
+					</thead>
+					<tbody>
 				 	<?php 
 				 	
 				 	function is_iterable($var)
@@ -113,30 +62,63 @@
 								<?php echo $sterm->title ?>
 							</td>
 							<td>
-								<?php echo $sterm->startdate ?>
+								<?php echo $sterm->start_date ?>
 							</td>
 							<td>
-								<?php echo $sterm->enddate ?>
+								<?php echo $sterm->end_date ?>
+							</td>
+							<td>
+								<?php echo $sterm->Terms ?>
 							</td>
 							
 							<td>
 									
-								<a href="edit/<?php echo urlencode($sterm->id); ?>"><?php echo $this->lang->line("editoption");?></a>
+								<a href="/schoolterms/newedit/<?php echo urlencode($sterm->marking_period_id); ?>"><?php echo $this->lang->line("editoption");?></a>
 							</td>
-							<td>
-								
-							</td>
+							
 						</tr>
 					<?php 
 						}
 					
 						}?>
+						</tbody>
 					</table>
-					<a href="add"><?php echo $this->lang->line("add_new_schoolterm");?></a>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+<!--div class="span9" id="content">
+  
+  			<div class="row-fluid">
+  				<div class="span4 navleft">
+  					<ul class="nav nav-pills nav-stacked">
+  						<li ><a href=""><?php echo $this->lang->line("search");?>&nbsp;<i class="icon-search icon-white"></i></a></li>
+  						<li class="active"><a href="../schoolterms/listing"><?php echo $this->lang->line("school_terms");?></a></li> 	
+  						<li><a href="../gradelevels/listing"><?php echo $this->lang->line("grade_levels");?></a></li>  
+  						<li><a href="../schoolclasses/listing"><?php echo $this->lang->line("schoolclasses");?></a></li>
+  						<li><a href="../schoolperiods/listing"><?php echo $this->lang->line("school_periods");?></a></li> 	
+  						<li><a href="../schoolsubjects/listing"><?php echo $this->lang->line("school_subjects");?></a></li> 			  						
+  					</ul>
+  					<div class="well">
+  						<p><b><?php echo $this->lang->line("help");?></b>&nbsp;<?php echo $this->lang->line("sample_help_message");?></p>
+  					</div>
+  				</div>
+  				
+        		<div class="span8">
+					<h1><?php echo $this->lang->line("school_terms");?></h1>
+					
+					<div class="btn-group">
+					
+                    	</div>
         		</div>  			
   				  				
   			</div>
-  		</div>
   		
-	</body>
-</html>
+  		
+</div>-->
